caebb829a9143d909e9a30dfeaf31f73103e760d
AndrewBerdyshev <an.berdyshev@gmail.com>
2024-10-09 20:19:51 +0700

    test added

--- min1.py
+++ min1.py
@@ -6,4 +6,13 @@ def bitsCount(n: int) -> int:
         count += n & 1
         n >>= 1
     return count
-print(bitsCount(int(input())))
+
+def check(arg: int, res: int) -> bool:
+    temp = bitsCount(arg)
+    if temp != res:
+        print(f"Function failed:\nArgument: {arg}\nExpected result: {res}\nActual result: {temp}\n")
+        return False
+    return True
+
+if check(10, 2) + check(-123, 3) == 2:
+    print("All tests passed!\n")
