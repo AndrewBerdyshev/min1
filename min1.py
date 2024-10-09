8ef4c3e8cd021444af13b6de03beaab292bf0682
AndrewBerdyshev <94905317+AndrewBerdyshev@users.noreply.github.com>
2024-10-09 11:53:56 +0700

    Update min1.py

--- min1.py
+++ min1.py
@@ -1,9 +1,8 @@
 def bitsCount(n: int) -> int:
     count = 0
     if n < 0:
-        n = ~n
-        n += 1
-    while n != 0:
+        count += 1
+    while n != 0 and n != -1:
         count += n & 1
         n >>= 1
     return count
